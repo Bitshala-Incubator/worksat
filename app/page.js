299583f5a6eac2ac478516ee3722d6913ec9797b
keraliss <keralis1131f@gmail.com>
2023-09-17 23:33:03 +0530

    next setup

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   
+   <>
+   <div className='flex justify-center'>
+   <div className='text-5xl text-gray-300 font-sans font-bold'>Upsats</div>
+   </div>
+   </>
+  )
+}
